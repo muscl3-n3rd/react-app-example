91eee494981324518beb7448f4fc4434c50ce6b8
Gonzalo Gisbert <muscl3.n3rd.3003@gmail.com>
2019-07-10 00:01:18 +0200

    adding data files and new contact us page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,11 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
 import DishDetailComponent from './DishdetailComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -14,6 +18,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
 
@@ -26,7 +33,10 @@ class Main extends Component {
 
         const HomePage = () => {
             return(
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
             )
         };
 
@@ -36,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> }/>
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home"/>
                 </Switch>
                 <Footer/>
