3a6444b9326b2c7c127ca472354fc9bd1c605ad5
Gonzalo Gisbert <muscl3.n3rd.3003@gmail.com>
2019-07-10 00:27:03 +0200

    adding some dishDetailsComponent fixes

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React  from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
    function RenderDish({ dish }) {
         return (
@@ -17,6 +17,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     function RenderComments({comments}){
+        console.log(comments)
         return (
             <div className="col-md-5 col-xm-12 col-xs-12 m-1">
                 <h4>Comments</h4>
@@ -44,9 +45,25 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     const DishDetail = ( props ) => {
         if(props.dish) {
             return (
-                <div className="row">
-                    <RenderDish dish ={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12" >
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderDish dish ={props.dish} />
+                        <RenderComments comments={props.comments} />
+                    </div>
                 </div>
             )
         }else{
@@ -54,8 +71,6 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                 <div></div>
             )
         }
-    }
-
-
+    };
 
 export default DishDetail;
