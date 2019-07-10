816748e8a5245ca21fba06f70337bebd31d05d35
Gonzalo Gisbert <muscl3.n3rd.3003@gmail.com>
2019-07-10 17:11:17 +0200

    adding fixes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes'
 import { COMMENTS } from '../shared/comments'
 import { LEADERS } from '../shared/leaders'
@@ -57,6 +58,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> }/>
                     <Route path="/menu/:dishId" component={DishWithId}/>
                     <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/> }/>
                     <Redirect to="/home"/>
                 </Switch>
                 <Footer/>
